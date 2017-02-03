0a07b27bd3d545a76e45d8f229b501463fc1d43b
danik <me@subsonic.cz>
2017-02-03 01:23:23 +0100

    removed css prefixes, added more customisation options

--- src/js/Nittro/Extras/Flashes/Service.js
+++ src/js/Nittro/Extras/Flashes/Service.js
@@ -1,4 +1,4 @@
-_context.invoke('Nittro.Extras.Flashes', function (DOM, Arrays) {
+_context.invoke('Nittro.Extras.Flashes', function (DOM, Arrays, CSSTransitions) {
 
     var Service = _context.extend(function (options) {
         this._ = {
@@ -21,6 +21,10 @@ _context.invoke('Nittro.Extras.Flashes', function (DOM, Arrays) {
 
         }
 
+        if (typeof this._.options.positioningOrder === 'string') {
+            this._.options.positioningOrder = this._.options.positioningOrder.split(/\s*,\s*|\s+/g);
+        }
+
         if (!this._.options.classes) {
             this._.options.classes = DOM.getData(this._.options.layer, 'flash-classes');
 
@@ -31,60 +35,57 @@ _context.invoke('Nittro.Extras.Flashes', function (DOM, Arrays) {
                 layer: null,
                 minMargin: 20,
                 positioning: null,
+                positioningOrder: 'above,rightOf,below,leftOf',
                 classes: null
             },
-            basicPositioning: [
-                function(target, elem, minMargin) {
+            basicPositioning: {
+                above: function (target, elem, minMargin, force) {
                     var res = {
-                        name: 'below',
-                        left: target.left + (target.width - elem.width) / 2,
-                        top: target.bottom
-                    };
-
-                    if (target.bottom + elem.height + minMargin < window.innerHeight && res.left > 0 && res.left + elem.width < window.innerWidth) {
-                        return res;
-
-                    }
-                },
-                function (target, elem, minMargin) {
-                    var res = {
-                        name: 'rightOf',
-                        left: target.right,
-                        top: target.top + (target.height - elem.height) / 2
-                    };
-
-                    if (target.right + elem.width + minMargin < window.innerWidth && res.top > 0 && res.top + elem.height < window.innerHeight) {
-                        return res;
-
-                    }
-                },
-                function (target, elem, minMargin) {
-                    var res = {
-                        name: 'above',
                         left: target.left + (target.width - elem.width) / 2,
                         top: target.top - elem.height
                     };
 
-                    if (target.top > elem.height + minMargin && res.left > 0 && res.left + elem.width < window.innerWidth) {
+                    if (force || target.top > elem.height + minMargin && res.left > 0 && res.left + elem.width < window.innerWidth) {
                         return res;
 
                     }
                 },
-                function (target, elem, minMargin) {
+                below: function(target, elem, minMargin, force) {
                     var res = {
-                        name: 'leftOf',
+                        left: target.left + (target.width - elem.width) / 2,
+                        top: target.bottom
+                    };
+
+                    if (force || target.bottom + elem.height + minMargin < window.innerHeight && res.left > 0 && res.left + elem.width < window.innerWidth) {
+                        return res;
+
+                    }
+                },
+                leftOf: function (target, elem, minMargin, force) {
+                    var res = {
                         left: target.left - elem.width,
                         top: target.top + (target.height - elem.height) / 2
                     };
 
-                    if (target.left > elem.width + minMargin && res.top > 0 && res.top + elem.height < window.innerHeight) {
+                    if (force || target.left > elem.width + minMargin && res.top > 0 && res.top + elem.height < window.innerHeight) {
+                        return res;
+
+                    }
+                },
+                rightOf: function (target, elem, minMargin, force) {
+                    var res = {
+                        left: target.right,
+                        top: target.top + (target.height - elem.height) / 2
+                    };
+
+                    if (force || target.right + elem.width + minMargin < window.innerWidth && res.top > 0 && res.top + elem.height < window.innerHeight) {
                         return res;
 
                     }
                 }
-            ]
+            }
         },
-        add: function (target, type, content, rich) {
+        add: function (content, type, target, rich) {
             type || (type = 'info');
 
             var elem = DOM.create('div', {
@@ -96,7 +97,8 @@ _context.invoke('Nittro.Extras.Flashes', function (DOM, Arrays) {
 
             }
 
-            var classes = target ? DOM.getData(target, 'flash-classes') : this._.options.classes;
+            var classes = target ? DOM.getData(target, 'flash-classes') : this._.options.classes,
+                inline = target ? DOM.getData(target, 'flash-inline') : false;
 
             if (classes) {
                 DOM.addClass(elem, classes.replace(/%type%/g, type));
@@ -112,16 +114,22 @@ _context.invoke('Nittro.Extras.Flashes', function (DOM, Arrays) {
 
             }
 
-            DOM.setStyle(elem, 'opacity', 0);
-            this._.options.layer.appendChild(elem);
+            var timeout = Math.max(2000, Math.round(elem.textContent.split(/\s+/).length / 0.003));
 
-            var style = {},
-                timeout = Math.max(2000, Math.round(elem.textContent.split(/\s+/).length / 0.003));
+            if (inline) {
+                target.appendChild(elem);
+                this._show(elem, 'inline', timeout);
+            }
 
             if (target) {
+                DOM.setStyle(elem, 'opacity', 0);
+                this._.options.layer.appendChild(elem);
+
                 var fixed = this._hasFixedParent(target),
                     elemRect = this._getRect(elem),
                     targetRect = this._getRect(target),
+                    style = {},
+                    positionName = DOM.getData(target, 'flash-position'),
                     position;
 
                 if (fixed) {
@@ -129,10 +137,17 @@ _context.invoke('Nittro.Extras.Flashes', function (DOM, Arrays) {
 
                 }
 
-                for (var i = 0; i < this._.options.positioning.length; i++) {
-                    if (position = this._.options.positioning[i].call(null, targetRect, elemRect, this._.options.minMargin)) {
-                        break;
+                if (positionName) {
+                    position = this._.options.positioning[positionName].call(null, targetRect, elemRect, this._.options.minMargin, true);
 
+                } else {
+                    for (var i = 0; i < this._.options.positioningOrder.length; i++) {
+                        positionName = this._.options.positioningOrder[i];
+
+                        if (position = this._.options.positioning[positionName].call(null, targetRect, elemRect, this._.options.minMargin)) {
+                            break;
+
+                        }
                     }
                 }
 
@@ -151,14 +166,15 @@ _context.invoke('Nittro.Extras.Flashes', function (DOM, Arrays) {
                     style.opacity = '';
 
                     DOM.setStyle(elem, style);
-                    this._show(elem, position.name, timeout);
+                    this._show(elem, positionName, timeout);
                     return;
 
+                } else {
+                    DOM.setStyle(elem, 'opacity', '');
                 }
             }
 
             this._.globalHolder.appendChild(elem);
-            DOM.setStyle(elem, 'opacity', '');
             this._show(elem, 'global', timeout);
 
         },
@@ -184,10 +200,9 @@ _context.invoke('Nittro.Extras.Flashes', function (DOM, Arrays) {
                 window.setTimeout(function () {
                     DOM.addClass(elem, 'nittro-flash-hide');
 
-                    window.setTimeout(function () {
+                    CSSTransitions.run(elem).then(function () {
                         elem.parentNode.removeChild(elem);
-
-                    }, 1000);
+                    });
                 }, timeout);
             }.bind(this);
 
@@ -227,5 +242,6 @@ _context.invoke('Nittro.Extras.Flashes', function (DOM, Arrays) {
 
 }, {
     DOM: 'Utils.DOM',
-    Arrays: 'Utils.Arrays'
+    Arrays: 'Utils.Arrays',
+    CSSTransitions: 'Utils.CSSTransitions'
 });
